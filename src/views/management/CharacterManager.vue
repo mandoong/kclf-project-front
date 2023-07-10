59672943eeff11ad57b029365b136c82e6df7160
mandoong <ljw0566@gmail.com>
2023-07-10 20:58:44 +0900

    feat: 캐릭터 목록 다중 삭제 기능

--- src/views/management/CharacterManager.vue
+++ src/views/management/CharacterManager.vue
@@ -4,6 +4,7 @@
     <ContentTable>
       <template v-slot:head>
         <tr class="h-10 truncate text-left">
+          <th class="pl-4 w-[5%]"></th>
           <th class="pl-4 w-[10%]">ID</th>
           <th class="px-4 w-[20%]">이름</th>
           <th class="px-4 w-[20%]">현재 득표수</th>
@@ -18,6 +19,22 @@
           :key="item"
           @click="$router.push(`/_admin/character/${item.id}`)"
         >
+          <td class="px-4 relative" @click.stop="onClickChecked(item.id)">
+            <div
+              class="border-2 rounded-sm w-5 h-5 mx-auto flex justify-center items-center"
+              :class="isChecked(item.id) ? 'bg-blue-400 border-blue-400' : ''"
+            >
+              <Transition
+                enter-from-class="opacity-0"
+                enter-active-class="transition-all"
+              >
+                <CheckIcon
+                  v-show="isChecked(item.id)"
+                  class="h-5 w-5 text-white"
+                />
+              </Transition>
+            </div>
+          </td>
           <td class="px-4">{{ item.id }}</td>
           <td class="px-4">{{ item.name }}</td>
           <td class="px-4">{{ item.vote_count }}</td>
@@ -27,12 +44,50 @@
         </tr>
       </template>
     </ContentTable>
+
+    <Transition
+      enter-from-class="opacity-0"
+      enter-active-class="transition-all"
+      leave-to-class="opacity-0"
+      leave-active-class="transition-all"
+    >
+      <div v-show="selectList.length" class="mt-10 w-full flex justify-end">
+        <button
+          class="bg-red-500 text-white rounded-md h-10 px-10"
+          @click="onDeleteCharacterModal = true"
+        >
+          삭제하기
+        </button>
+      </div>
+    </Transition>
+
+    <ManagerModal
+      :onModal="onDeleteCharacterModal"
+      :onButton="true"
+      @close="onDeleteCharacterModal = false"
+      @submit="onClickDeleteCharacter"
+    >
+      <div
+        class="h-32 flex justify-center items-center text-center text-red-500"
+      >
+        선택된 캐릭터를 삭제하시겠습니까? <br />
+        <br />
+        삭제된 캐릭터는 복구 할수 없습니다.<br />
+      </div>
+    </ManagerModal>
+    <ManagerModal :onModal="deleteSuccess">
+      <div class="flex justify-center items-center h-20 text-xl">
+        캐릭터가 삭제되었습니다.
+      </div>
+    </ManagerModal>
   </div>
 </template>
 
 <script>
 import ContentTable from "../../components/ContentTable.vue";
+import { CheckIcon } from "@heroicons/vue/24/outline";
 import Subtitle from "../../components/Subtitle.vue";
+import ManagerModal from "../../components/ManagerModal.vue";
 import dayjs from "dayjs";
 import { Auth, Character } from "../../service/Repository";
 
@@ -42,6 +97,9 @@ export default {
       nav: [{ name: "전체 영화 리스트", href: "/movie?page=1" }],
       characters: [],
       pageCount: null,
+      selectList: [],
+      onDeleteCharacterModal: false,
+      deleteSuccess: false,
     };
   },
 
@@ -59,7 +117,35 @@ export default {
     dateFormat(date) {
       return dayjs(date).format("YYYY-MM-DD  HH:MM");
     },
+
+    isChecked(id) {
+      return this.selectList.some((e) => e === id);
+    },
+
+    onClickChecked(id) {
+      if (this.selectList.some((e) => e === id)) {
+        this.selectList = this.selectList.filter((e) => e !== id);
+      } else {
+        this.selectList.push(id);
+      }
+    },
+
+    async onClickDeleteCharacter() {
+      this.onDeleteCharacterModal = false;
+
+      for (const id of this.selectList) {
+        await Character.DeleteCharacter(id);
+      }
+
+      this.deleteSuccess = true;
+      this.selectList = [];
+
+      setTimeout(() => {
+        this.deleteSuccess = false;
+        this.fetch();
+      }, 2000);
+    },
   },
-  components: { ContentTable, Subtitle, dayjs },
+  components: { ContentTable, Subtitle, dayjs, CheckIcon, ManagerModal },
 };
 </script>
