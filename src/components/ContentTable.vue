0c19850c443c2f6e3df9766544433113b2fa9118
mandoong <ljw0566@gmail.com>
2023-07-11 20:07:59 +0900

    feat: 테이블 크기 수정

--- src/components/ContentTable.vue
+++ src/components/ContentTable.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div
-      class="overflow-y-scroll relative max-h-[700px] shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg"
+      class="overflow-y-scroll relative max-h-[650px] shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg scrollbar-thin scrollbar-thumb-gray-300"
     >
       <table class="w-full">
         <thead class="bg-[#59677e] text-white sticky top-0 z-20">
